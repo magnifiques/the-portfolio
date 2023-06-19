bcca897d2f3d48d1a7a0cca15b94864d13e28c12
magnifiques <arpit.vaghela@icloud.com>
2023-06-20 01:45:15 +0530

    change the hero text size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[100px] lg:top-[10px] mx-auto grid lg:grid-cols-2 justify-items-center items-center gap-2 lg:gap-4   `}
+        className={`${styles.paddingX} absolute inset-0 top-[100px] lg:top-[10px] mx-auto grid lg:grid-cols-2 justify-items-center items-center gap-2 lg:gap-4`}
       >
         <div>
           {/* <div className="flex flex-col justify-center items-center mt-5">
--- src/styles.js
+++ src/styles.js
@@ -4,9 +4,9 @@ const styles = {
   padding: "sm:px-16 px-6 sm:py-16 py-10",
 
   heroHeadText:
-    "font-black text-white lg:text-[60px] sm:text-[40px] xs:text-[30px] text-[20px] lg:leading-[78px] mt-2",
+    "font-black text-white lg:text-[60px] sm:text-[50px] xs:text-[30px] text-[25px] lg:leading-[78px] mt-2",
   heroSubText:
-    "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[20px] xs:text-[15px] text-[10px] lg:leading-[30px]",
+    "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[25px] xs:text-[15px] text-[20px] lg:leading-[30px]",
 
   sectionHeadText:
     "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
